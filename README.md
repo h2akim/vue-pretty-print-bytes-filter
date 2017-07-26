211efaf5f969543058767194125d96504c3b3a94
Hakim Razalan <akimrr@gmail.com>
2017-07-26 15:53:59 +0800

    add precision to filter

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # vue-pretty-print-bytes-filter
 
+Fork of [james2doyle/vue-pretty-print-bytes-filter](https://github.com/james2doyle/vue-pretty-print-bytes-filter)
+
+Add precision options to the filter
+
+### Original README
+
 A filter for Vue.js to format bytes into a nice human-readable format. I pretty (no pun intended) much just stole this from [sindresorhus/pretty-bytes](https://github.com/sindresorhus/pretty-bytes).
 
 ### Install
--- vue.pretty-print-bytes.js
+++ vue.pretty-print-bytes.js
@@ -1,11 +1,14 @@
 /* globals Vue */
 
 // optional filter for formatting the bytes in the view
-Vue.filter('prettyBytes', function (num) {
+Vue.filter('prettyBytes', function (num, precision = 3) {
   // jacked from: https://github.com/sindresorhus/pretty-bytes
   num = Number(num);
   if (isNaN(num)) {
     throw new TypeError('Expected a number');
+  }
+  if (isNaN(precision)) {
+    precision = 3;
   }
 
   var neg = num < 0;
@@ -20,7 +23,7 @@ Vue.filter('prettyBytes', function (num) {
   }
 
   var exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
-  num = (num / Math.pow(1000, exponent)).toFixed(2) * 1;
+  num = (num / Math.pow(1000, exponent)).toFixed(precision) * 1;
   var unit = units[exponent];
 
   return (neg ? '-' : '') + num + ' ' + unit;
